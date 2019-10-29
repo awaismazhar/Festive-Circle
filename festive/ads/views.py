c25fb5d4ef9459a49491e6c2fb7593719bb98d42
denizerlive <da774ma774@gmail.com>
2019-10-29 23:16:18 +0500

    latest

--- festive/ads/views.py
+++ festive/ads/views.py
@@ -91,8 +91,10 @@ def main(request):
         return render(request,"main.html",context)
 
 def edit(request, id):
+
     detail = Detail.objects.get(id=id)
-    # location = Location.objects.get(id=detail.loction_id.id)
+    # loc = Location.objects.get(id=detail.loction_id.id)
+    # print(loc.id)
     image = images.objects.filter(detail_id=id)
     venue = Venue.objects.get(detail_id=id)
     venuePrice = VenuePrice.objects.get(venue_id=venue)
@@ -104,7 +106,50 @@ def edit(request, id):
        'venuePrices':venuePrice,
        'dishMenus':dishMenu,
     }
-    return render(request,"edit.html", context)
+    if request.method == 'POST':
+        post_title =request.POST.get('post_title')
+        phone =request.POST.get('phone')
+        city =request.POST.get('city')
+        area =request.POST.get('area')
+        street =request.POST.get('street')
+        post_description =request.POST.get('post_description')
+        guest_price =request.POST.get('guest_price')
+        ac_price =request.POST.get('ac_price')
+        heater_price =request.POST.get('heater_price')
+        dj_system_price =request.POST.get('dj_system_price')
+        decoration_price =request.POST.get('decoration_price')
+        dish_title =request.POST.get('dish_title')
+        dish_description =request.POST.get('dish_description')
+        price =request.POST.get('price')
+        sitting_capacity =request.POST.get('sitting_capacity')
+        parking_capacity =request.POST.get('parking_capacity')
+        decoration =request.POST.get('decoration')
+        wifi =request.POST.get('wifi')
+        valet_parking =request.POST.get('valet_parking')
+        heater =request.POST.get('heater')
+        ac =request.POST.get('ac')
+        dj_system =request.POST.get('dj_system')
+        bridal_room =request.POST.get('bridal_room')
+        generator =request.POST.get('generator')
+        outside_dj =request.POST.get('outside_dj')
+        outside_decoration =request.POST.get('outside_decoration')
+        outside_catering =request.POST.get('outside_catering')
+        category = request.POST.get('category')
+
+
+        Location.objects.filter(id=id).update(city=city,area=area,street=street)
+        Detail.objects.filter(id=id).update(title=post_title,phoneNo=phone,description=post_description)
+        for img in request.FILES.getlist('images'):
+            images.objects.filter(detail_id=id).update(title=post_title,image=img)
+        Venue.objects.filter(detail_id=id).update(sitting_capacity=sitting_capacity,category=category,parking_capacity=parking_capacity,air_conditioner=ac,heater=heater,dj_system=dj_system,wifi=wifi,bridal_room=bridal_room,valet_parking=valet_parking,decoration=decoration,generator=generator,outside_catering=outside_catering,outside_dj=outside_dj,outside_decoration=outside_decoration)
+        VenuePrice.objects.filter(venue_id=venue).update(per_guest=guest_price,air_conditioner=ac_price,heater=heater_price,dj_system=dj_system_price,decoration=decoration_price)
+        Dish_Menu.objects.filter(venue_id=venue).update(title=dish_title,description=dish_description,price=price)
+        
+        print('post update')
+        return redirect('/')
+
+    else:
+        return render(request,"edit.html", context)
 
 def delete(request,id):
     location = Location.objects.get(id=id)
@@ -144,8 +189,6 @@ def add(request):
         outside_decoration =request.POST.get('outside_decoration')
         outside_catering =request.POST.get('outside_catering')
         category = request.POST.get('category')
-
-        
 
 
         location = Location.objects.create(city=city,area=area,street=street)
